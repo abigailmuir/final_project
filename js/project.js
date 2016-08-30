02abac2986915086595860988b9f07395c71a859
Abigail Muir <abby@motivatedesign.com>
2016-08-30 18:41:48 -0400

    Added new stuff

---
+++ js/project.js
@@ -0,0 +1,16 @@
+
+// Sorry that I didn't include javascript :( I really wanted to but as you can see based on this project, work has completely consumed all of my free time for the last month.
+
+
+// $("document").ready(function(){
+
+
+
+// 	$("goblue").hover(function(){
+// 		$("h4color").css({
+// 			color: red,
+// 		});
+// 	});
+
+
+// });
